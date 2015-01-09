97c2272e19c90c893e20877e5237c82637d6213e
steveuk <unknown@example.com>
2015-01-09 22:53:10 +0000

    Joust
    Former-commit-id: 34761bd3660ee85f77f6bbf310ca5a8e9336841e

---
+++ lua/gamemodes/battleroyale/mutators/joust.lua
@@ -0,0 +1,22 @@
+local mutator = {}
+
+mutator.Base = "default_bullets"
+mutator.Name = "#JB_BR_RoundTypeJousting_Title"
+mutator.Description = "#JB_BR_RoundTypeJousting_Desc"
+
+mutator.ItemPool = { }
+
+function mutator:ScaleHitboxDamage( pl, hitbox, info )
+
+	if( info:GetDamageCustom( JB_DMG_CUSTOM_DIVING ) ) then
+		info:ScaleDamage( 50 )
+		info:ScaleDamageForce( 150 )
+		return true
+	end
+
+	return false
+	
+end
+
+mutators:Register( "duckhunt", mutator )
+
