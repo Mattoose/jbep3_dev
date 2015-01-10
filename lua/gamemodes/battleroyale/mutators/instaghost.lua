e0e812beb5b940f6be6365f63aae1fac54cb6dd6
John McBroom <jimbomcb@gmail.com>
2015-01-10 08:43:31 +0000

    instaghost firerate increased over time
    Former-commit-id: ad1566b9adeda14a7a5f28405d8de0e67b2ac06c

--- lua/gamemodes/battleroyale/mutators/instaghost.lua
+++ lua/gamemodes/battleroyale/mutators/instaghost.lua
@@ -1,4 +1,6 @@
 local mutator = {}
+local nextUpdateTime = 0
+local startTime = 0
 
 mutator.Base = "default"
 mutator.Name = "#JB_BR_RoundTypeInstaGhosts_Title"
@@ -15,10 +17,20 @@ mutator.ItemPool = {
 
 function mutator:RoundStart()
 	FindConVar( "jb_sv_fartsteps" ):SetValue( true )
+	startTime = CurTime()
+	nextUpdateTime = CurTime() + 5.0
 end
 
 function mutator:RoundEnd()
 	FindConVar( "jb_sv_fartsteps" ):Revert()
+end
+
+function mutator:Think()
+	-- Increase over time
+	if CurTime() > nextUpdateTime then
+		nextUpdateTime = CurTime() + 5
+		FindConVar( "jb_sv_ig_firerate" ):SetValue( math.RemapValClamped( CurTime() - startTime, 0, 80, 1.0, 0.01 ) )
+	end
 end
 
 mutators:Register( "instaghosts", mutator )
