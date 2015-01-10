e1d42ee847fa20f96f4d16691a59490b787de43f
steveuk <unknown@example.com>
2015-01-10 21:22:52 +0000

    Randomly spawning scientists
    Former-commit-id: f92232b7074c5e50ccf78f3f239eca2be201bc33

--- lua/gamemodes/battleroyale/mutators/science.lua
+++ lua/gamemodes/battleroyale/mutators/science.lua
@@ -9,3 +9,30 @@ mutator.PlayerModels = { "models/player/bms_scientist.mdl" }
 
 mutators:Register( "science", mutator )
 
+function mutator:RoundStart()
+	nextScientistSpawn = CurTime() + 30
+end
+
+function mutator:Think()
+
+	if( CurTime() >= nextScientistSpawn ) then
+		local randomPlace = navmesh.GetRandomEmptyNavArea()
+
+		if( randomPlace ) then
+			local scientist = CreateEntityByName( "npc_scientist_chase")
+			local pos = randomPlace:GetCenter() + Vector( 0, 0, 12 )
+			local hidingSpots = randomPlace:GetHidingSpots()
+
+			if( hidingSpots and #hidingSpots > 0 ) then
+				pos = hidingSpots[ math.random( #hidingSpots ) ].pos
+			end
+
+			scientist:SetAbsOrigin( pos )
+			scientist:Spawn()
+			scientist:DropToFloor()
+		end
+
+		nextScientistSpawn = CurTime() + math.random( 7, 15 )
+	end
+
+end
