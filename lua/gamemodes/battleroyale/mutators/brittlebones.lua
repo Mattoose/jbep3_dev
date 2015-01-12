452e032817fb84256515a85437e352f13d46f633
postal <unknown@example.com>
2015-01-12 23:40:45 +0000

    new brittlebones value for wall impact
    Former-commit-id: 8188283ba6811724fe860f480f37402df8a5a7c6

--- lua/gamemodes/battleroyale/mutators/brittlebones.lua
+++ lua/gamemodes/battleroyale/mutators/brittlebones.lua
@@ -6,17 +6,15 @@ mutator.Description = "Extreme wall/ceiling impact damage."
 --mutator.PlayerModels = { "models/player/skeleton/skeleton.mdl" } -- Re-enable when we can use skeleton model
 
 function mutator:RoundStart()
-	FindConVar( "jb_sv_impact_wallspeed" ):SetValue( 30 )
+	FindConVar( "jb_sv_impact_wallspeed" ):SetValue( 64 )
 	FindConVar( "jb_sv_impact_ceilspeed" ):SetValue( 30 )
 	FindConVar( "jb_sv_impact_damage" ):SetValue( 500 )
-	FindConVar( "jb_sv_impact_wallspeed" ):SetValue( 30 )
 end
 
 function mutator:RoundEnd()
 	FindConVar( "jb_sv_impact_wallspeed" ):Revert()
 	FindConVar( "jb_sv_impact_ceilspeed" ):Revert()
 	FindConVar( "jb_sv_impact_damage" ):Revert()
-	FindConVar( "jb_sv_impact_wallspeed" ):Revert()
 end
 
 mutators:Register( "brittlebones", mutator )
