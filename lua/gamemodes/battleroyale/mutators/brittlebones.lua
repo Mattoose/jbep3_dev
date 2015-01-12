03d19c7900655fc49901c3b0a93af9607b6089fa
John McBroom <jimbomcb@gmail.com>
2015-01-12 05:41:13 +0000

    Added brittle bones mutator
    Former-commit-id: e193477a4cff73d901a01a689770dfd617425bf1

---
+++ lua/gamemodes/battleroyale/mutators/brittlebones.lua
@@ -0,0 +1,23 @@
+local mutator = {}
+
+mutator.Base = "default"
+mutator.Name = "Brittle Bones"
+mutator.Description = "Extreme wall/ceiling impact damage."
+--mutator.PlayerModels = { "models/player/skeleton/skeleton.mdl" } -- Re-enable when we can use skeleton model
+
+function mutator:RoundStart()
+	FindConVar( "jb_sv_impact_wallspeed" ):SetValue( 30 )
+	FindConVar( "jb_sv_impact_ceilspeed" ):SetValue( 30 )
+	FindConVar( "jb_sv_impact_damage" ):SetValue( 500 )
+	FindConVar( "jb_sv_impact_wallspeed" ):SetValue( 30 )
+end
+
+function mutator:RoundEnd()
+	FindConVar( "jb_sv_impact_wallspeed" ):Revert()
+	FindConVar( "jb_sv_impact_ceilspeed" ):Revert()
+	FindConVar( "jb_sv_impact_damage" ):Revert()
+	FindConVar( "jb_sv_impact_wallspeed" ):Revert()
+end
+
+mutators:Register( "brittlebones", mutator )
+
